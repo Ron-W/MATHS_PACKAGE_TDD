a902a879c9608eab779f9400cc78113ebbf15c58
Ron-W <30573501+Ron-W@users.noreply.github.com>
2017-08-20 11:29:10 +0100

    First commit

---
+++ LINEAR_TDD/src/rwMaths.c
@@ -0,0 +1,34 @@
+/*
+/ rwMaths.c
+
+// includes a library of mathematical functions
+*/
+
+//#include "C:/Users/Ron/Documents/dev/rwCLib/rwMaths.h"
+//#include "../../../../rwCLib/rwMaths.h" //=> No such file or directory
+//#include "../../../../../../rwCLib/rwMaths.h" // => No such file or directory
+#include "rwMaths.h"
+
+uint32_t calcYfromX(uint32_t x)
+{
+	return (uint32_t)0;
+}
+
+char returnChar(void)
+{
+	return 'p';
+}
+
+
+char mathsChar(void)
+{
+	char asdf = 'b';
+	return asdf;
+}
+/*
+uint32_t calcYfromX(uint32_t x)
+{
+	uint32_t val = x;
+	return val;
+}
+*/
---
+++ LINEAR_TDD/src/rwMaths.h
@@ -0,0 +1,18 @@
+/************************************************************************/
+/* rwMaths.h															*/
+/*                                                                      */
+/************************************************************************/
+
+#ifndef RWMATHS_H
+#define RWMATHS_H
+
+#include <stdint.h>
+
+uint32_t calcYfromX(uint32_t x);
+
+char returnChar(void);
+char mathsChar(void);
+
+//uint32_t calcYfromX(uint32_t x);
+
+#endif	// RWMATHS_H
